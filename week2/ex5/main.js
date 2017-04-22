20a43f03674ebf7c887fa5aa869fdf2f9176a046
Erwin Heba <erwin0917@gmail.com>
2017-04-22 18:17:45 +0200

    add ex5

--- week2/ex5/main.js
+++ week2/ex5/main.js
@@ -1,100 +1,46 @@
+(function() {
 
-   
-   function EventEmitter() {
- 
-    this.events = {};
- 
-}
- 
-EventEmitter.prototype.on = function(type, fn) {
- 
-    if(!type || !fn) return;
- 
-    this.events[type] = this.events[type] || [];
-    this.events[type].push(fn);
- 
-}
- 
-EventEmitter.prototype.emit = function(type, data) {
- 
-    var fns = this.events[type];
- 
-    if(!fns || !fns.length) return;
- 
-    for(var i = 0; i < fns.length; i++) {
-        fns[i](data);
+    function debounce(fn, time) {
+        // docelowo powinna być wywoływana
+        // funkcja fn w ten sposób fn()
+
+        var wait = false;
+        
+        // część kodu tutaj
+
+        return function() {
+            // pozostały kod tutaj
+            if( !wait ){
+                wait = true;
+                
+                
+                clearTimeout(timeout);  
+                var timeout = setTimeout(function(){
+                    wait = false;
+                    fn.apply(this, arguments);
+                    
+                }, time || 300)
+            }
+            
+        };
     }
- 
-};
- 
-function Database(url) {
-    
-    EventEmitter.call(this); // Przypisanie this z EventEmitter na this z Datebase
-    
-    this.url = url;
-    
- 
-}
 
+    var handleScroll = debounce(function() {
+        console.log("Scrollujemy!");
+    }, 300);
 
-// ------ Dodanie dziedziczenia z EventEmitter
-Database.prototype = Object.create(EventEmitter.prototype);
-Database.prototype.constructor = Database;
+    // w tym miejscu pod handleScroll
+    // powinna być nowa funkcja
 
+    // Przypisanie zdarzenia "scroll"
+    window.addEventListener("scroll", handleScroll, false);
 
+    // Możesz również przetestować funkcję
+    // ze zdarzeniem "resize" skalując okno
+    var handleResize = debounce(function() {
+        console.log("Zmieniamy rozmiar okna!");
+    }, 500);
 
-Database.prototype.connect = function() {
- 
-    // fikcyjne podłączenie do bazy
- 
-    this.emit("connect", this.url);
- 
-}
- 
-Database.prototype.disconnect = function() {
- 
-    // fikcyjne zakończenie połączenia z bazą
- 
-    this.emit("disconnect", this.url);
- 
-}
- 
-// Użycie EventEmittera
-var ev = new EventEmitter();
- 
-ev.on("hello", function(message) {
-    console.log("Witaj " + message + "!");
-});
- 
-ev.on("hello", function(message) {
-    console.log("Siema " + message + ".");
-});
- 
-ev.on("goodbye", function() {
-    console.log("Do widzenia!");
-});
- 
-ev.emit("hello", "Marek");
-ev.emit("goodbye");
-ev.emit("custom"); // nic się nie wydarzy
- 
-// DO ZROBIENIA!
-// Docelowe użycie klasy Database
-var db = new Database("db://localhost:3000"); // fikcyjny adres
- 
-db.on("connect", function(url) {
-    console.log("Połączenie z bazą pod adresem " + url + " zostało ustanowione.");
-});
- 
-db.on("disconnect", function(url) {
-    console.log("Połączenie z bazą pod adresem " + url + " zostało zakończone.");
-});
+    window.addEventListener("resize", handleResize, false);
 
-db.connect();
- 
-// po 5 sekundach rozłączamy się
-setTimeout(function() {
-    db.disconnect();
-}, 5000);
-
-    
+})();
