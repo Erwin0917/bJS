19f61c65143feada94d0c3744862bfb6fda7da46
Erwin Heba <erwin0917@gmail.com>
2017-05-19 21:28:09 +0200

    w6 add ex2

---
+++ week6/ex1/main.js
@@ -0,0 +1,77 @@
+//(function(){
+
+//Accept: application/json
+
+function getJSON(url, successFn, errorFn) {
+    var xhr = new XMLHttpRequest(),
+        data = null;
+
+    xhr.open("GET", url, true);
+
+
+
+    xhr.onreadystatechange = function() {
+
+        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
+            data = JSON.parse(xhr.response);
+            successFn(data);
+        }
+
+    };
+
+    xhr.onabort = function() { errorFn(xhr.status); };
+    xhr.onerror = function() {
+        errorFn(xhr.status)
+    };
+
+
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.send(null); //wysyłamy zapytanie - po wyslaniu nie mozemy juz nic z obiektem zrobic
+
+
+}
+
+
+let url = "http://code.eduweb.pl/bootcamp/json/";
+
+
+
+
+let output = document.querySelector(".output");
+getJSON(url, function(data) {
+
+    const personAll = new WeakMap();
+
+
+    window.personAll = personAll; // do sprawdzania co jest w personAll w konsoli
+
+    for (let person of data) {
+        let li = document.createElement("li");
+        let { name, email } = person;
+        li.innerHTML = name;
+        output.appendChild(li);
+
+
+        personAll.set(li, email);
+        li.addEventListener("click", function(e) {
+            if (li.innerHTML === name) {
+                li.innerHTML = `${name} <a href=mailto:${email}>${email}</a>`;
+            } else li.innerHTML = name;
+
+
+        }, false);
+    }
+
+
+
+
+
+
+}, function(err) {
+    console.log("Wystąpił błąd!");
+    console.log(err);
+});
+
+
+//})();
---
+++ week6/ex2/main.js
@@ -0,0 +1,49 @@
+Array.myFrom = function(elem, fn, ts) {
+    
+    var elemArr = [],
+        length = elem.length;
+
+    for(var i = 0; i < length; i++ ){
+        elemArr.push(elem[i])
+
+    };
+
+    
+    
+
+    if(elem !== undefined && fn === undefined){
+        return elemArr;
+    }else if(typeof(fn) === "function") {
+        
+        var mapArr = elemArr.map(fn);
+
+        fn.bind(this);
+        return mapArr;
+
+    }
+
+    
+};
+
+
+
+
+
+
+
+/////////////////////////////////////////////
+
+var lis = document.querySelectorAll("ul li");
+
+var lisArr = Array.myFrom(lis);
+
+console.log(lisArr);
+
+console.log(Array.isArray(lisArr)); // true
+
+var lisTexts = Array.myFrom(lis, function(li) {
+    return li.textContent;
+});
+
+console.log(lisTexts);
+// lisTexts powinna być tablicą z tekstami wszystkich <li>
